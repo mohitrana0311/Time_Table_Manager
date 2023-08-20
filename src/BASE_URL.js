ba1a434d27362705160c2cd96a734b796c73fa78
mohitrana0311 <95418289+mohitrana0311@users.noreply.github.com>
2023-08-20 23:00:24 +0530

    Update BASE_URL.js

--- src/BASE_URL.js
+++ src/BASE_URL.js
@@ -1,4 +1,4 @@
-export const BASE_URL="https://tangerine-custard-9985df.netlify.app"
+export const BASE_URL="https://wondrous-gelato-ccc8f7.netlify.app"
 
 // export const BASE_URL="https://serene-starship-71e66b.netlify.app"
 
